3a2d3374722ef21377fd6904fef6d99209534973
Joe Gerard <167501805+joegerard8@users.noreply.github.com>
2025-02-07 14:56:55 -0700

    adding mission 5 to the repo

---
+++ Mission5/WebApplication1/wwwroot/js/serviceCalculator.js
@@ -0,0 +1,16 @@
+﻿// takes inputs for time etc to calculate how much I would charge for college basketball services
+
+$(document).ready(function () {
+    $('#calculate').click(function () {
+        let hours = $('#hours').val();
+
+        if (hours >= 1 && hours <= 24) {
+            let totalCost = hours * 50;
+            $('#totalCost').text('Total Cost: $' + totalCost.toFixed(2))
+                .css("display", "block"); // Explicitly set display
+            console.log(totalCost);
+        } else {
+            alert("Please enter a valid number between 1-24 hours.");
+        }
+    });
+});
